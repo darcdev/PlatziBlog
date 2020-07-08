93bf6a39c029ab1d1e28e1f1fdeec6d48ab290ec
darcdev <diegoandresrojas2000@gmail.com>
2020-07-08 18:18:08 -0500

    create store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore({},{} )
 
 ReactDOM.render(
 	<App />,
